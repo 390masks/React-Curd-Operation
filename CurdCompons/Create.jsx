3c34863cf1a3eb38d5ab5f9ae5bfa77dcc775b72
Abhinav A Ashok <64303979+390masks@users.noreply.github.com>
2025-03-06 13:39:08 +0530

    Update Create.jsx

--- CurdCompons/Create.jsx
+++ CurdCompons/Create.jsx
@@ -3,50 +3,67 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Create = () => {
-  const [values,setValues]=useState({
-    name:'',
-    email:'',
-    phone:''
-  })
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  });
 
-  const navigate=useNavigate();
- 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    axios.post("http://localhost:3000/users",values)
-    .then(respones=>{console.log(respones);
-      navigate('/')})
-    .catch(error=>console.error(error))
-  }
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3000/users", values)
+      .then(response => {
+        console.log(response);
+        navigate('/');
+      })
+      .catch(error => console.error(error));
+  };
 
   return (
     <div>
-      <div>
-        <h1>Provide your details</h1>
-        <form action="" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name</label><br />
-            <input type="text" placeholder='Enter your name'
-            onChange={(e)=>setValues({...values,name:e.target.value})}/>
-          </div>
-          <div>
-            <label htmlFor="">E-mail</label><br />
-            <input type="text" placeholder='Enter your Email'
-            onChange={(e)=>setValues({...values,email:e.target.value})}/>
-          </div>
-          <div>
-            <label htmlFor="">phone</label><br />
-            <input type="text"  placeholder='Enter your Phone'
-            onChange={(e)=>setValues({...values,phone:e.target.value})}/>
-          </div>
-          <div>
-            <button type='submit'>Add </button>
-             <button><Link to='/'>Back</Link></button>
-             </div>
-        </form>
-      </div>
+      <h1>Provide your details</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label><br />
+          <input 
+            type="text" 
+            id="name" 
+            placeholder="Enter your name"
+            value={values.name}
+            onChange={(e) => setValues({ ...values, name: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">E-mail</label><br />
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Enter your Email"
+            value={values.email}
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone</label><br />
+          <input 
+            type="tel" 
+            id="phone" 
+            placeholder="Enter your Phone"
+            value={values.phone}
+            onChange={(e) => setValues({ ...values, phone: e.target.value })}
+          />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+          <Link to="/">
+            <button type="button">Back</button>
+          </Link>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
