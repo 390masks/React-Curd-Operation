09385e4a6de83d52f53a5637fe88550caac3dd07
Abhinav A Ashok <64303979+390masks@users.noreply.github.com>
2025-03-07 13:20:16 +0530

    Update Read.jsx

--- CurdCompons/Read.jsx
+++ CurdCompons/Read.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Read = () => {
-  return (
-    <div>Read</div>
-  )
-}
+  const [data, setData] = useState({});
+  const { id } = useParams(); 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/users/${id}`) 
+      .then((response) => setData(response.data))
+      .catch((error) => console.error("Error in fetching:", error));
+  }, [id]); 
 
-export default Read
+  return (
+    <div>
+      <h2>User Details</h2>
+      <div>
+        <strong>Name: </strong>{data.name}
+      </div>
+      <div>
+        <strong>Email: </strong>{data.email}
+      </div>
+      <div>
+        <strong>Phone: </strong>{data.phone}
+      </div>
+      <div>
+        <button><Link to={`/update/${id}`}>Edit</Link></button> 
+        <button><Link to='/'>Back</Link></button>
+      </div>
+    </div>
+  );
+};
+
+export default Read;
